98a5249fb84997c2f33218628ccea6f603ffaaea
valentyn <v.kormilets@gmail.com>
2019-11-04 23:04:14 +0200

    Add results_page

--- HW8/function.php
+++ HW8/function.php
@@ -1,4 +1,5 @@
 <?php
+$start_a = microtime(true);
 $name = $_POST['name'];
 $mail = $_POST['mail'];
 $os = $_POST['radio'];
@@ -7,19 +8,21 @@ echo "Ты любишь работать на $os" . "<br/>";
 $fizz = $_POST['fizz'];
 $buzz = $_POST['buzz'];
 $num = $_POST['num'];
-fizzbuzz($fizz, $buzz, $num);
 function fizzbuzz ($f, $b, $n){
-    for ($i = 1; $i <= $n; $i++){
-        if (($i % $f == 0) && ($i % $b == 0)){
-            echo "FB ";
+    if (!empty($f) && !empty($b) && !empty($n)) {
+        for ($i = 1; $i <= $n; $i++) {
+            if (($i % $f == 0) && ($i % $b == 0)) {
+                echo "FB ";
+            } elseif ($i % $b == 0) {
+                echo "B ";
+            } elseif ($i % $f == 0) {
+                echo 'F ';
+            } else echo $i . " ";
         }
-        elseif ($i % $b == 0){
-            echo "B ";
-        }
-        elseif ($i % $f == 0){
-            echo 'F ';
-        }
-        else echo $i . " ";
     }
-    return true;
+        else echo "Проверка FizzBuzz невозможна,т.к. введены не все данные";
+        return true;
 }
+//echo "<br/>";
+require_once ('results_page.html');
+//echo microtime(true) - $start_a;
