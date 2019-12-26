b2bee08059424a9b6441a33c7819f6b41b2bd489
valentyn <v.kormilets@gmail.com>
2019-12-27 01:19:43 +0200

    Some change

--- HW_20/views/index.php
+++ HW_20/views/index.php
@@ -22,7 +22,7 @@
         tsec=today%60; today=Math.floor(today/60); if(tsec<10)tsec='0'+tsec;
         tmin=today%60; today=Math.floor(today/60); if(tmin<10)tmin='0'+tmin;
         thour=today%24; today=Math.floor(today/24);
-        timestr=today +" дней "+ thour+" часов "+tmin+" минут "+tsec+" секунд";
+        timestr=today +" дня "+ thour+" часов "+tmin+" минут "+tsec+" секунд";
         document.getElementById('t').innerHTML=timestr;
         window.setTimeout("time()",1000);
     }
